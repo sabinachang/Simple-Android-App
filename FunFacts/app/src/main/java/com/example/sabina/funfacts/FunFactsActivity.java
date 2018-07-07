cd387edd997ba9a01e9e0ea26b43a8c6694bd9ff
sabinachang <sabchang1@gmail.com>
2018-07-07 21:12:04 +0800

    Basic FunFactApp completes.

--- FunFacts/app/src/main/java/com/example/sabina/funfacts/FunFactsActivity.java
+++ FunFacts/app/src/main/java/com/example/sabina/funfacts/FunFactsActivity.java
@@ -1,13 +1,48 @@
 package com.example.sabina.funfacts;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
-
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
+    private FactBook factBook = new FactBook();
+    private ColorWheel colorWheel = new ColorWheel();
+    private TextView factTextView;
+    private Button showFactButton;
+    private RelativeLayout relativeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+        relativeLayout = findViewById(R.id.relativeLayout);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v){
+                String fact = factBook.getFact();
+                int color = colorWheel.getColor();
+
+                // Update screen with new app
+                factTextView.setText(fact);
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+
+        };
+        showFactButton.setOnClickListener(listener);
+        //Toast.makeText(this,"Yeah activity created",Toast.LENGTH_SHORT).show();
+        Log.d(TAG,"From onCreate");
     }
 }
